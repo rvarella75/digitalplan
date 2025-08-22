b178f092f789276c3fd0ad6933f47af9e7a76685
rvarella75 <renato.varella@gmail.com>
2025-08-22 17:00:59 -0500

    Disable all CSP to troubleshoot eval() error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta httpEquiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;" />
+        {/* CSP disabled to troubleshoot eval() error */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
